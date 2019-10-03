4a23a7c12f75944ac0c13d8a17d58967134ba14b
JaySebagh <jaysebagh@gmail.com>
2019-10-02 17:06:40 -0700

    day 1 mvp

--- src/player.py
+++ src/player.py
@@ -2,11 +2,36 @@
 # currently.
 
 class Player:
-    def __init__(self, name):
-        self.name = name
-
-    def __str__(self):
-        return f"name: {self.name}"
+    def __init__(self, player, room):
+        self.player = player
+        self.room = room
     
     def __repr__(self):
-        return f'room({repr(self.name)})'
+        return f'({repr(self.player)}, {repr(self.room)})'
+
+    def move(self, command):
+        moveList = ["n", "e", "s", "w"]
+        try:
+            moveList.index(command)
+            if(command == "q"):
+                return
+            
+            thisRoom = getattr(self.room, command + "_to")
+            if(thisRoom):
+                self.room = thisRoom
+            else:
+                print("No room found")
+            self.room.onRoomChange()
+        except:
+            print("Failed")
+            pass
+
+        # print(self.room.n_to.name)
+        # self.room = self.room[command + "_to"]
+
+
+
+
+
+        # if command in moveList:
+        #     command
--- src/room.py
+++ src/room.py
@@ -5,6 +5,14 @@ class Room:
     def __init__(self, name, description):
         self.name = name
         self.description = description
+        self.n_to = None
+        self.e_to = None
+        self.s_to = None
+        self.w_to = None
+    
+    def onRoomChange(self):
+        print(self.name)
+        print(self.description)
 
     def __str__(self):
         return f"name: {self.name} \ndescription: {self.description}"
